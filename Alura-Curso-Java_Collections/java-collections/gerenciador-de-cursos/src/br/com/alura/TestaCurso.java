fe3838e3145842c895a0d8e4e34d7aa502aef26c
Felipe Matheus Ribeiro da Siva <felisilva@pit.local>
2022-09-11 20:55:28 -0300

    adicionado anotações no curso de coleções java

---
+++ Alura-Curso-Java_Collections/java-collections/gerenciador-de-cursos/src/br/com/alura/TestaCurso.java
@@ -0,0 +1,42 @@
+package br.com.alura;
+
+import java.util.Collections;
+import java.util.Comparator;
+
+public class TestaCurso {
+	
+	public static void main(String[] args) {
+		Curso geografia = new Curso("Geografia", "Paulo Almeida");
+		
+		Aula aula = new Aula("Aula - 1", 10);
+		Aula aula2 = new Aula("Aula - 2", 30);
+		Aula aula3 = new Aula("Aula - 3", 15);
+		Aula aula4 = new Aula("Capitais do Brasil", 5);
+		
+		geografia.addAula(aula);
+		geografia.addAula(aula2);
+		geografia.addAula(aula3);
+		geografia.addAula(aula4);
+		
+		//Utiliza o método do Comparator para ordenar a lista
+		geografia.getAulas().sort(Comparator.comparing(Aula::getTempo));
+		
+		//Ordena utilizando o Collections, cujo critério de ordenamento é definido pela implementação do
+		//método CompareTo();
+		Collections.sort(geografia.getAulas());
+		
+		for(Aula aulaSelected : geografia.getAulas()) {
+			System.out.println(aulaSelected);	
+		}
+		
+		//Lacra a lista, não permitindo que a mesma seja modificada, fica disponível
+		//Apenas para leitura
+		geografia.setAulas(Collections.unmodifiableList(geografia.getAulas()));
+		
+		geografia.addAula(aula4);
+		
+		
+	}
+
+}
+
